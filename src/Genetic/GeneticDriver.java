1bfc31be4a28d280184672917238163865b23466
Albert <albertortizguillem97@gmail.com>
2017-11-13 22:47:35 +0100

    Initial commit

---
+++ src/Genetic/GeneticDriver.java
@@ -0,0 +1,8 @@
+package Genetic;
+import Genetic.GeneticBase;
+public class GeneticDriver {
+    public static void main(String[] args) {
+        GeneticBase gb = new GeneticBase();
+        gb.geneticAlgorithm();
+    }
+}
---
+++ src/Genetic/Solution.java
@@ -0,0 +1,17 @@
+package Genetic;
+
+public class Solution {
+    private Pair puntuation = new Pair(0,0);
+    private Individual ind;
+
+    public Solution(Pair punt, Individual individual){
+        ind = individual;
+        puntuation = punt;
+
+    }
+
+    public Individual getIndividual(){return ind;}
+
+    public Pair getPuntuation(){return puntuation;}
+
+}
